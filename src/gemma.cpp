c871f71756144cdff12b2b3516176c30ad150275
Pjotr Prins <pjotr.public01@thebird.nl>
2017-07-13 08:54:17 +0000

    LOCO: use anno file

--- src/gemma.cpp
+++ src/gemma.cpp
@@ -311,7 +311,8 @@ void GEMMA::PrintHelp(size_t option) {
 
   if (option==4) {
     cout<<" RELATEDNESS MATRIX CALCULATION OPTIONS" << endl;
-    cout<<" -loco     [chr]          "<<" leave one chromosome out (LOCO) by name" << endl;
+    cout<<" -loco     [chr]          "<<" leave one chromosome out (LOCO) by name (also use annotation file)" << endl;
+    cout<<" -a        [filename]     "<<" specify input BIMBAM SNP annotation file name (LOCO only)"<<endl;
     cout<<" -gk       [num]          "<<" specify which type of kinship/relatedness matrix to generate (default 1)" << endl;
     cout<<"          options: 1: centered XX^T/p"<<endl;
     cout<<"                   2: standardized XX^T/p"<<endl;
